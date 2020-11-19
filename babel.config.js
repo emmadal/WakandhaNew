384a72120317e6de59a99699a54840bd6186db1f
bunyy <umangrathore70@gmail.com>
2020-11-19 18:06:15 +0530

    Android Code.

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    ['@babel/plugin-proposal-optional-catch-binding'],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+};
